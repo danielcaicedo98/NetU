5ea60759073c257160a21f1a49d97d06966b8abc
danielcaicedo98 <daniel.andres.caicedo@correounivalle.edu.co>
2021-03-18 14:24:36 -0500

    Agregando GUI inicial Cliente

---
+++ NetU_Cliente/src/netu_cliente/NetU_Cliente.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netu_cliente;
+
+import vista.LoginGUI;
+import vista.LoginGUI1;
+import vista.PrincipalGUI;
+
+/**
+ *
+ * @author PuntoPC
+ */
+public class NetU_Cliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        LoginGUI login = new LoginGUI();
+        login.setVisible(true);
+        
+        LoginGUI1 login1 = new LoginGUI1();
+        login1.setVisible(true);
+        
+        //PrincipalGUI pr = new PrincipalGUI();
+        //pr.setVisible(true);
+        
+    }
+    
+}
