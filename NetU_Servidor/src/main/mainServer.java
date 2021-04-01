399fef063564e81597a683a209529f5cc7a19909
Camila <camila.sanchez@correounivalle.edu.co>
2021-04-01 18:38:40 -0500

    creacion paquete modelo y sus clases

--- NetU_Servidor/src/main/mainServer.java
+++ NetU_Servidor/src/main/mainServer.java
@@ -1,5 +1,8 @@
 package main;
 
+import vista.LoginServidor;
+import vista.Servidor;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
--- NetU_Servidor/src/modelo/EmpleadoDAO.java
+++ NetU_Servidor/src/modelo/EmpleadoDAO.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
+package modelo;
 
 /**
  *
- * @author USUARIO
+ * @author PuntoPC (Camila)
  */
-public class x {
+public class EmpleadoDAO {
+    
     
 }
