8fe3da1fd20fe00a25339e297631ca4c72ffc878
danielcaicedo98 <daniel.andres.caicedo@correounivalle.edu.co>
2021-04-05 01:56:43 -0500

    Corrección

--- NetU_Servidor/src/conexion/Fachada.java
+++ NetU_Servidor/src/conexion/Fachada.java
@@ -17,20 +17,14 @@ public class Fachada {
     
     private static Connection con = null;
     
-    /*usr = root
-    pwd = 
-    driver = com.mysql.cj.jdbc.Driver
-    url = jdbc:mysql://localhost:3306/sistemanetu?characterEncoding=utf8
-    */
+    
     
     public static Connection getConnection(){
         try
         {
             if(con == null){
                 
-                Runtime.getRuntime().addShutdownHook(new MiShDwnHook());
-                /*ResourceBundle rb = ResourceBundle.getBundle("src.conexion.jdbc");
-                String driver = rb.getString("driver");*/
+                Runtime.getRuntime().addShutdownHook(new MiShDwnHook());                
                 String url = "jdbc:mysql://localhost:3306/sistemanetu?characterEncoding=utf8";
                 String pwd = "";
                 String usr = "root";
